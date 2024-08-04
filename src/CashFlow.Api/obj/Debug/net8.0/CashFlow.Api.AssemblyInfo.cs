949d850feaab356be451526484a190539e82bd73
Pedro Esmerio <pedro.esmerio@gatec.com.br>
2024-08-04 00:51:34 -0300

    fix(hotfix): removing unused codeAdd LICENSE.txt.

--- src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cdbb9b2861cf51fa0444417b92c476dda238ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0202455f250d1500047366d3a983922909f81c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
